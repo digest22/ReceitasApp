24bb91575773394902d7d07b1d127cef67d9df2c
digest22 <daniloficha99@gmail.com>
2019-07-08 11:19:17 -0300

    Avanço nas telas. Cadastro e visializar faltando tempo de preparo e rendimento. Xbotões. Xadicionar imagem. Xsalvar.

---
+++ app/src/main/java/com/example/aluno/receitas/CadastroReceitasActivity.java
@@ -0,0 +1,13 @@
+package com.example.aluno.receitas;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CadastroReceitasActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastro_receitas);
+    }
+}
--- app/src/main/java/com/example/aluno/receitas/MainActivity.java
+++ app/src/main/java/com/example/aluno/receitas/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.aluno.receitas;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,5 +14,25 @@ ImageButton imgBtnCadastro;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imgBtnCadastro = (ImageButton) findViewById(R.id.imgBtnCadastro);
+
+        imgBtnCadastro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                launchCadastro();
+
+
+
+            }
+        });
+
+
+
+
+    }
+    private void launchCadastro(){
+        Intent intent = new Intent(this, CadastroReceitasActivity.class);
+        startActivity(intent);
     }
 }
